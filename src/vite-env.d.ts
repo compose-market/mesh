b46da4d643ef58e2b3ed19bb0876b5007aa6ab14
jabyl <jabyl.new@gmail.com>
2026-03-31 14:58:43 +0200

    Massive redesign of 'mission control' + 'agent dashboard'

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,6 @@
 interface ImportMetaEnv {
   readonly VITE_THIRDWEB_CLIENT_ID: string;
   readonly VITE_API_URL?: string;
-  readonly VITE_LAMBDA_URL?: string;
 }
 
 interface ImportMeta {
@@ -12,3 +11,9 @@ interface ImportMeta {
 
 declare module "react-simple-maps";
 declare module "world-atlas/countries-110m.json";
+
+declare const __MESH_FLEET_NODES__: ReadonlyArray<{
+  peerId: string;
+  provider: string;
+  region: string;
+}>;
